bd540fa1afd63934bd2f960585d08f086d89810f
remarqueqiqi <remarqueminmin@live.com>
2016-09-10 12:55:54 -0400

    all

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    return String(html);
+     
+  }
+};
